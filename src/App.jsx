b741d3352ce5fec958e18f04807c6731f8bbc333
feven-t <feventesfaye623@gmail.com>
2024-03-11 12:55:24 +0300

    Structure and layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,35 @@
+import styles from "./style"
+import { Billing, Business, CardDeal, Clients,CTA, Footer, Navbar, Stats, Testimonials, Hero } from "./components";
+
 const App = () => {
   return (
-    <div className='text-blue-600'>
-      <p>hello</p>
+    <div className='bg-primary text-blue-600'>
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          Navbar
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          Hero
+        </div>
+      </div>
+
+
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Stats />
+        <Business />
+        <Billing />
+        <CardDeal />
+        <Testimonials />
+        <Clients/>
+        <CTA />
+        <Footer />
+      </div>
+    </div>
+
     </div>
   )
 }
---
+++ src/components/Testimonials.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Testimonials = () => {
+  return (
+    <div>
+      Testimanilas
+    </div>
+  )
+}
+
+export default Testimonials
